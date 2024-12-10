9db877fa7e5361019ed748de516fefab49bf1867
Huan-Rong <zhenghuanrong@yeah.net>
2024-12-10 23:02:57 +0800

    Add new file: p11-e1.8.cpp

---
+++ ch1/p11-e1.8.cpp
@@ -0,0 +1,15 @@
+// p11-e1.8.cpp
+#include <iostream>
+
+int main()
+{
+	// std::cout << "/*";
+	// std::cout << "*/";
+	// std::cout << /* "*/" */;
+	// std::cout << /*  "*/" /*  "/*"  */;
+	
+	// try other versions: who is the beginner?
+	// std::cout << "/*"/**/;
+	std::cout << /*"*/"*/";
+	return 0;
+}
